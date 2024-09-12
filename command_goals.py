e41009769d47316c6ab7446d71f11a6de004f87b
Eli <elifish607@gmail.com>
2024-09-12 17:37:12 +0300

    Finishing a project

--- command_goals.py
+++ command_goals.py
@@ -1,24 +1,73 @@
 from models import Aircrafts, Pilots, CityTarget, Command_goals
-import main
+
 
 
 # יוצר רשימה בעלת כל האופציות האפשריות למטרה
-def command_goals():
+def command_goals(aircraft_list, pilots_list, city_target_list):
     goals_list = []
-    for aircraft in main.aircraft_list:
-        for pilot in main.pilots_list:
-            for city_target in  main.city_target_list:
+    for city_target in city_target_list:
+        for pilot in pilots_list:
+            for aircraft in aircraft_list:
                 goals = Command_goals.Command_goals(
-                    target_city = city_target[]
-                    priority = priority
-                    assigned_aircraft = assigned_aircraft
-                    distance = distance
-                    weather_conditions = weather_conditions
-                    pilot_skill = pilot_skill
-                    aircraft_capacity = aircraft_capacity
-                    fuel_capacity = fuel_capacity
-                    mission_fit_scores = mission_fit_scores
+                    target_city = city_target.city,
+                    priority = city_target.priority,
+                    assigned_pilot = pilot.name,
+                    assigned_aircraft = aircraft.type,
+                    distance = city_target.distanse,
+                    weather_conditions = city_target.weather_conditions,
+                    weather = city_target.weather,
+                    pilot_skill = pilot.skill_level,
+                    aircraft_speed = aircraft.speed,
+                    fuel_capacity = aircraft.fuel_capacity,
+                    mission_fit_scores = mission_fit_scores\
+                        (score_distance(city_target_list , city_target.distanse)\
+                         , score_aircraft_type(aircraft_list, aircraft.fuel_capacity )\
+                         , score_pilot_skill(pilots_list, pilot.skill_level)\
+                         , score_weather_conditions(city_target_list, city_target.weather)\
+                         , score_execution_time()\
+                         , score_priority(city_target_list, city_target.priority))
                 )
                 goals_list.append(goals)
+
+    goals_list.sort(key=lambda x: x.mission_fit_scores , reverse=True)
+    return goals_list
+
+
+# מייצרת ציוני התאמה למשימה
+def mission_fit_scores(distanse, aircraft_type, pilot_skill, weather_conditions, execution_time, priority):
+    mission_fit_scores = ((distanse * 0.15)/
+           + (aircraft_type * 0.20)/
+           + (pilot_skill * 0.20)/
+           + (weather_conditions * 0.20)/
+           + (execution_time * 0.10)/
+           + (priority * 0.15))
+    return mission_fit_scores * 1000
+
+# פןנקציית עזר ליצירת ציון ספציפי מאחד עד עשר עבור כל פרמטר
+
+def score_distance(city_target_list, distanse):
+    max_skill_level = max(city_target_list, key=lambda x: x.distanse)
+    return (float(distanse)/ float(max_skill_level.distanse)) *10
+
+
+def score_aircraft_type(aircraft_list, fuel_capacity):
+    max_fuel_capacity = max(aircraft_list, key=lambda x: x.fuel_capacity)
+    return (float(fuel_capacity) / float(max_fuel_capacity.fuel_capacity)) * 10
+
+def score_pilot_skill(pilots_list, pilot_skill):
+    max_pilot_skill = max(pilots_list, key=lambda x: x.skill_level)
+    return (float(pilot_skill) / float(max_pilot_skill.skill_level)) * 10
+
+def score_weather_conditions(city_target_list, weather):
+    max_city_target_list = max(city_target_list, key=lambda x: x.weather)
+    return (float(weather) / float(max_city_target_list.weather)) * 10
+
+def score_execution_time():
+    return 100
+
+
+def score_priority(city_target_list, priority):
+    max_priority = max(city_target_list, key=lambda x: x.priority)
+    return (float(priority) / float(max_priority.priority)) * 10
 
 
--- main.py
+++ main.py
@@ -1,26 +1,68 @@
 import requests
 import json
+from command_goals import command_goals
 from  load_functions import load_aircrafts_from_file, load_pilots_from_file, load_city_targets_from_csv
 import requests
-# קריאת המטוסים לליסט
-aircraft_list = load_aircrafts_from_file()
 
-for aircraft in aircraft_list:
-    print(f"Type: {aircraft.type}, Speed: {aircraft.speed}, Fuel Capacity: {aircraft.fuel_capacity}")
+# משתנים למקרה שעוד לא הוטען הקובץ
+aircraft_list = []
+pilots_list = []
+city_target_list = []
+goals_list = []
 
-
-# קריאת המטרות לליסט
-pilots_list = load_pilots_from_file()
-
-for pilots in pilots_list:
-    print(f"name: {pilots.name}, skill_level: {pilots.skill_level}")
-
-# קריאת  המטרות לליסט
-city_target_list = load_city_targets_from_csv()
+# יצירת אינדקס
+bool = True
+while bool:
+    chous = int(input(" אנא בחר פעולה: \n 1 - טעינת קבצים \n 2 - הצגת טבלת המלצה לתקיפות\n 3 - שמירת כל התקיפות לקובץ \n 4 - יציאה \n :"))
 
 
-for city_target in city_target_list:
-    print(f"City: {city_target.city}, Priority: {city_target.priority} ,  distanse: {city_target.distanse} , weather: {city_target.weather} , weather_conditions: {city_target.weather_conditions}" )
+    if chous == 1:
+        # קריאת המטוסים לליסט
+        aircraft_list = load_aircrafts_from_file()
+
+        # קריאת המטרות לליסט
+        pilots_list = load_pilots_from_file()
+
+        # קריאת  המטרות לליסט
+        city_target_list = load_city_targets_from_csv()
+
+        for aircraft in aircraft_list:
+            print(f"Type: {aircraft.type}, Speed: {aircraft.speed}, Fuel Capacity: {aircraft.fuel_capacity}")
+
+        for pilots in pilots_list:
+            print(f"name: {pilots.name}, skill_level: {pilots.skill_level}")
+
+        for city_target in city_target_list:
+            print(
+                f"City: {city_target.city}, Priority: {city_target.priority} ,  distanse: {city_target.distanse} , weather: {city_target.weather} , weather_conditions: {city_target.weather_conditions}")
+    if chous == 2:
+        # יצירת רשימת מטרות
+        goals_list = command_goals(aircraft_list, pilots_list, city_target_list)
+        for goals in goals_list:
+            print(f" target_city: {goals.target_city},  \
+             priority: {goals.priority},  \
+              assigned_pilot: {goals.assigned_pilot},  \
+               assigned_aircraft: {goals.assigned_aircraft},  \
+                distance: {goals.distance},  \
+                 weather_conditions: {goals.weather_conditions},  \
+                 weather: {goals.weather},  \
+                  pilot_skill: {goals.pilot_skill},  \
+                   aircraft_speed: {goals.aircraft_speed},  \
+                    fuel_capacity: {goals.fuel_capacity},  \
+                     mission_fit_scores: {goals.mission_fit_scores}")
+    if chous == 3:
+        print("קובץ נטען")
+
+
+    if chous == 4:
+        bool = False
+
+
+
+
+
+
+
 
 
 
